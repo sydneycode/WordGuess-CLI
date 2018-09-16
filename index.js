fa1d5c3c1de7b2268351094adba65aa8c8770052
sydneycode <sstaggs12@gmail.com>
2018-09-16 13:40:37 -0700

    started working on the index file for the game

---
+++ index.js
@@ -0,0 +1,11 @@
+// Require the "inquirer" npm package for receiving user input
+var inquirer = require("inquirer");
+
+// Require the Word module exported from Word.js
+var Word = require("./Word.js");
+
+// Randomly select a word from a list of words and use the "Word"
+// constructor to store it
+
+// Prompt the user for each letter guess and keep track of the 
+// user's remaining guesses
